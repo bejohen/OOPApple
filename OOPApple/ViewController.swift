3a4a6ef2ad1b507a479e1681f81051da3da4f15d
Johanes Steven <bejohen@gmail.com>
2019-05-15 08:14:13 +0700

    Initial Commit

---
+++ OOPApple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOPApple
+//
+//  Created by Johanes Steven on 15/05/19.
+//  Copyright © 2019 bejohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
